fcb20e16d073df02b16db48b43d5fa14b1bd5b05
Rafael Poveda <raerpo@gmail.com>
2017-11-03 11:47:31 -0300

    initial commit

---
+++ background.ts
@@ -0,0 +1,6 @@
+// chrome.pageAction.onClicked.addListener((tab) => {
+//   chrome.tabs.executeScript({
+//     code: `document.querySelector("body").innerHTML = "hello world"`
+//   })
+// });
+console.log('hello from background');
